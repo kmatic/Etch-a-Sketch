5d0eed23ab856702b7b868cb5617bd8d9caa18c6
Kristopher <kristopher_matic@hotmail.com>
2022-07-12 21:44:47 -0700

    Added size button which allows for variable grid resolution

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
+let gridSize = 16;
 const container = document.querySelector(".container");
+const sizeButton = document.querySelector("#size");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        container.appendChild(cell);
+sizeButton.addEventListener("click", () => gridResize());
+
+function gridResize() {
+    do {
+        gridSize = prompt("Please enter a value for squares per side (max 100)");
+    } while (gridSize <= 0 || gridSize > 100);
+    container.innerHTML = "";
+    setGrid(gridSize);
+}
+
+function setGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.addEventListener("mouseover", () => {
+                cell.classList.add("hover");
+            })
+            container.appendChild(cell);
+        }
     }
 }
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => {
-    cell.addEventListener("mouseover", () => {
-        cell.classList.add("hover");
-    })
-})
+setGrid(gridSize);
